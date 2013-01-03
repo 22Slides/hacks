dbea59c59e5e330d4d65cf7b3742497215501e13
Bryan Buchanan <bryan@resen.co>
2013-01-03 08:42:39 -0800

    Added hack to put gallery on text page

---
+++ Gallery on Text Page/js.js
@@ -0,0 +1,11 @@
+rsn.dropdown();
+slideshowDelay = 2;
+
+// Load gallery page into team page
+$('p:contains("PHOTOS_GO_HERE")').each(function() {
+	$(this).replaceWith('<div class="gallery"></div>');
+	var photosPage = $('body').attr('id').replace('page-team', '/photos');
+	$('div.gallery').load(photosPage + " .thumbs", function(data) { 
+		$(this).find('a').attr('target', '_blank');
+	});
+});
