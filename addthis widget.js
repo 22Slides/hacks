bc1a202c395f96e252d717c503cc22d3f1296d35
Bryan Buchanan <bryan@resen.co>
2013-07-14 17:44:59 -0700

    updates

---
+++ addthis widget.js
@@ -0,0 +1,3 @@
+// AddThis Widget
+$('footer').prepend('<div class="addthis_toolbox addthis_default_style" style="margin-bottom:5px;"><a class="addthis_counter addthis_pill_style"></a></div>');
+$('<script>').prop('src', '//s7.addthis.com/js/300/addthis_widget.js#pubid=undefined').appendTo('body');
