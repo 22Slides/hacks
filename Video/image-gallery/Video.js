e98b020ffbe4e0963c9dc101427d654add4fb440
Bryan Buchanan <bryan@resen.co>
2014-02-28 13:33:14 -0800

    Video update

--- Video/image-gallery/Video.js
+++ Video/image-gallery/Video.js
@@ -1,6 +1,10 @@
 /* Videos */
 $(document).ready(function() {
-	$('.images iframe').closest('li').each(function() {
+	$('.images iframe').css({
+		'float': 'left',
+		'max-height': '96%',
+		'max-width': '100%'
+	}).closest('li').each(function() {
 		$(this).find('img').remove();
 		$(this).find('iframe').prependTo($(this));
 		$(this).find('div').remove();
