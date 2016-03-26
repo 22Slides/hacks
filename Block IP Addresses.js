b511c7dbeb03336e012ee9321a7a01636e743502
Bryan Buchanan <bryan@resen.co>
2016-03-25 23:42:36 -0700

    update

--- Block IP Addresses.js
+++
@@ -1,5 +0,0 @@
-// IP Blocker
-var ip = ['167.206.244.21', '167.206.244.10'];
-$.each(ip, function(key, value) {
-	if (clientIP == value) window.location = "http://google.com";
-});
--- No Scrollwheel click downloading.js
+++
@@ -1,5 +0,0 @@
-/* Stop scrollwheel click downloading */
-$('.thumbs a').each(function() {
-	var href = "#" + $(this).attr('href');
-	$(this).attr('href', href);
-});
