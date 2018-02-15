cc14c2c07f8ba17ccc43c62221316c0d49c17823
Bryan <bryan@resen.co>
2018-02-14 17:32:49 -0800

    Update

--- Gallery on Text Page/js.js
+++
@@ -1,8 +0,0 @@
-// Load gallery page into team page
-$('p:contains("PHOTOS_GO_HERE")').each(function() {
-	$(this).replaceWith('<div class="gallery"></div>');
-	var photosPage = $('body').attr('id').replace('page-team', '/photos');
-	$('div.gallery').load(photosPage + " .thumbs", function(data) { 
-		$(this).find('a').attr('target', '_blank');
-	});
-});
